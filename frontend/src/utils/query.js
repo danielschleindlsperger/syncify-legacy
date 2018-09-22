33ba77ab78961f1386e297b52e61c5a513d1c16d
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2018-09-23 00:42:55 +0200

    use correct jest matcher for object equality; fix query module

--- frontend/src/utils/query.js
+++ frontend/src/utils/query.js
@@ -3,13 +3,25 @@ import * as R from 'ramda'
 // :: Object -> String
 const locationHref = R.path(['location', 'href']);
 
-// Reads query parameters from window.location and returns as key/value
+// :: Array -> Boolean
+const lessThanTwoLong = R.pipe(R.length, R.lt(R.__, 2))
+
+// :: Array -> Array
+const handleMissingQueryDelimiter = R.when(
+  lessThanTwoLong,
+  R.always(['', ''])
+)
+
+// Reads query parameters from window.location and returns as
+// key/value inside a POJO. Does not handle Arrays.
 // :: window -> Object
 export const query = definitelyNotWindow => R.pipe(
   locationHref,
   R.split('?'),
+  handleMissingQueryDelimiter,
   R.last,
   R.split('&'),
+  R.reject(R.isEmpty), // filter out empty strings as object keys
   R.map(R.split('=')),
   R.fromPairs,
 )(definitelyNotWindow);
--- frontend/src/utils/query.test.js
+++ frontend/src/utils/query.test.js
@@ -3,16 +3,26 @@ import { query } from './query'
 describe('query', () => {
   it('extracts non duplicated query parameters to object', () => {
     const mockWindow = { location: { href: 'http://test.com?foo=bar&fu=gazi' }}
-    expect(query(mockWindow)).toEqual({ foo: 'bar', fu: 'gazi' })
+    expect(query(mockWindow)).toStrictEqual({ foo: 'bar', fu: 'gazi' })
   })
 
   it('sets missing values to undefined', () => {
     const mockWindow = { location: { href: 'http://test.com?keyWithMissingValue' }}
-    expect(query(mockWindow)).toEqual({ keyWithMissingValue: undefined })
+    expect(query(mockWindow)).toStrictEqual({ keyWithMissingValue: undefined })
   })
 
   it('returns empty object if no query string is present', () => {
     const mockWindow = { location: { href: 'http://test.com' }}
-    expect(query(mockWindow)).toEqual({})
+    expect(query(mockWindow)).toStrictEqual({})
+  })
+
+  it('returns empty object if nothing comes after query string delimiter', () => {
+    const mockWindow = { location: { href: 'http://test.com?' }}
+    expect(query(mockWindow)).toStrictEqual({})
+  })
+
+  it('handles ampersand at last position', () => {
+    const mockWindow = { location: { href: 'http://test.com?foo=bar&' }}
+    expect(query(mockWindow)).toStrictEqual({ foo: 'bar' })
   })
 })
