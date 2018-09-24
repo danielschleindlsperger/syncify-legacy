25dbf82753d0b9edcb6aea97221c7752cea8f68e
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2018-09-24 22:21:54 +0200

    add bundle analyzer

--- frontend/src/config.js
+++
@@ -1,7 +0,0 @@
-// TODO: store such things in global .env files
-// Use the ones from backend but move to mono repo root
-
-export const config = {
-  API_URL: process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api' : 'https://syncify.com',
-  LOGIN_PATH: '/auth/login',
-}
