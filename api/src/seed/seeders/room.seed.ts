227f6f1f6d3c88d2ba63720a291baf94d81589ce
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2018-12-31 01:39:42 +0100

    fix room seeder problems forrealnow

--- api/src/seed/seeders/room.seed.ts
+++ api/src/seed/seeders/room.seed.ts
@@ -4,7 +4,9 @@ import { Seeder } from '../seeder'
 import { range } from 'ramda'
 import { fakeData } from '../../tests/mocks'
 
-const roomSeed: Partial<Room>[] = range(0, 50).map(() => fakeData())
+const roomSeed: () => Promise<Partial<Room>>[] = () => range(0, 50).map(() => fakeData())
 
-export const seedRoomTable: Seeder = (connection: Connection) =>
-  connection.getRepository('room').save(roomSeed)
+export const seedRoomTable: Seeder = async (connection: Connection) => {
+  const seed = await Promise.all(roomSeed())
+  connection.getRepository('room').save(seed)
+}
