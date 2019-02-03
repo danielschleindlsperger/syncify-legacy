47921d33428911cc5cbf1e8539e53eb6ea29f39e
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2019-02-03 21:53:45 +0100

    send multiple requests to spotify api for playlists longer than 50 to avoid limits

--- frontend/src/api/spotify.js
+++ frontend/src/api/spotify.js
@@ -1,4 +1,5 @@
 import SpotifyWebApi from 'spotify-web-api-js'
+import { pipe, concat, map, splitEvery } from 'ramda'
 
 export const spotifyFactory = accessToken => {
   const spotify = new SpotifyWebApi()
@@ -6,20 +7,25 @@ export const spotifyFactory = accessToken => {
   return spotify
 }
 
-export const setWebPlayerAsActiveDevice = (accessToken, deviceId) => {
-  console.log('setWebPlayerAsActiveDevice', accessToken, deviceId)
-  return spotifyFactory(accessToken).transferMyPlayback([deviceId], {
+export const setWebPlayerAsActiveDevice = (accessToken, deviceId) =>
+  spotifyFactory(accessToken).transferMyPlayback([deviceId], {
     // in case user is already listening on another device we need to stop playback
     play: false,
   })
-}
 
 export const getMyPlaylists = accessToken => spotifyFactory(accessToken).getUserPlaylists()
 
 export const getPlaylist = (accessToken, playlistId) =>
   spotifyFactory(accessToken).getPlaylist(playlistId)
 
-export const getSongs = (accessToken, songIds) =>
-  spotifyFactory(accessToken)
-    .getTracks(songIds)
-    .then(x => x.tracks)
+export const getSongs = (accessToken, songIds = []) => {
+  const spotify = spotifyFactory(accessToken)
+  const tracks = Promise.all(splitEvery(50, songIds).map(spotify.getTracks)).then(
+    pipe(
+      map(x => x.tracks),
+      trackLists => trackLists.reduce(concat),
+    ),
+  )
+
+  return tracks
+}
