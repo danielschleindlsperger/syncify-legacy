f70a6a6b4d02e3cfc2d970f47c0fcc96400495e1
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2019-02-03 21:22:29 +0100

    make login page a little less ugly and remove dumb wording

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { HeadlineLarge, Text } from '../components/typography'
 
 const Login = () => (
   <div style={{ width: 600, maxWidth: '100%', margin: '50px auto' }}>
-    <h1>Login</h1>
-    <h3>Please log into my humble site. You will be redirected to Spotify and then again here.</h3>
-    <p>We'll wait for you ❤</p>
-    <a href="/api/auth/login">Off you go.</a>
+    <HeadlineLarge>Login</HeadlineLarge>
+    <Text style={{ display: 'block', marginTop: 20 }}>
+      Unfortunately you can't use Syncify without logging into your Spotify account first. You will
+      be redirected to Spotify and then again here.
+    </Text>
+    <a style={{ display: 'block', marginTop: 20 }} href="/api/auth/login">
+      Login
+    </a>
   </div>
 )
 
