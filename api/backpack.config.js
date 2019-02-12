410e8f4a5a1522b47ca370a36a23deadd0227a02
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2019-02-13 00:03:39 +0100

    don't minify api bundle

--- api/backpack.config.js
+++ api/backpack.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   webpack: (config, options, webpack) => {
+    // don't minify in order to keep timestamp for migration files
+    config.optimization.minimize = false
+
     config.entry.main = ['./src/index.ts']
 
     config.resolve = {
