1d1e1da2630e27b71d29ed75c03ee788882a29a9
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2019-05-04 14:34:48 +0200

    remove old deployment configs

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,10 +1,6 @@
 module.exports = {
-  cache: false,
   setupTestFrameworkScriptFile: '<rootDir>tests/setupTests.js',
   testEnvironment: 'jest-environment-jsdom-global',
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-  },
   transformIgnorePatterns: [
     // node_modules that should be transpiled
     '/node_modules/(?!styled-icons).+\\.js$',
