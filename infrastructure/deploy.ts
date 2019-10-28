a21483fc3b89e58ed6b6cab1ab6790da02511399
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2019-10-28 22:01:10 +0100

    don't cache index.html for very long

--- infrastructure/deploy.ts
+++ infrastructure/deploy.ts
@@ -37,7 +37,7 @@ function uploadFrontendAssets() {
       Body: fs.readFileSync(filePath),
       ACL: 'public-read',
       ContentType,
-      CacheControl: assetKey === 'index.html' ? 'no-cache' : 'max-age=31536000',
+      CacheControl: assetKey === 'index.html' ? 'max-age=2' : 'max-age=31536000',
     }
     await s3.putObject(params).promise()
     console.log(`Successfully uploaded ${assetKey}.`)
